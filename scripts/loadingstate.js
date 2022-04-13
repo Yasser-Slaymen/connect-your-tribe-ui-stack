8132ba9a16e3462bdce7c4da982e4fc27aa54cc4
Yasser-Slaymen <yaserslyman0@gmail.com>
2022-04-13 13:17:51 +0200

    update

--- scripts/loadingstate.js
+++ scripts/loadingstate.js
@@ -1,4 +1,15 @@
 let sprinnerWrapper = document.querySelector('.spinner-wrapper')
-window.addEventListener('load',() =>{
-    sprinnerWrapper.parentElement.removeChild(sprinnerWrapper);
-})
+
+setTimeout(() =>{
+    hidePreloader();
+}, 1000)
+
+function hidePreloader() {
+    sprinnerWrapper.style.display = "none"
+}
+
+// the function without timer in stution internet slow work
+// let sprinnerWrapper = document.querySelector('.spinner-wrapper')
+// window.addEventListener('load',() =>{
+//     sprinnerWrapper.parentElement.removeChild(sprinnerWrapper);
+// })
